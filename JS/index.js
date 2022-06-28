58b6948a418c5b8e2ae34db076801b98510613b7
Marcelo Cardoso Filho <marcelobuenocardosofilho@gmail.com>
2022-06-28 16:22:53 -0300

    Novos Arquivos

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+var btn = document.querySelector('#show');
+var container = document.querySelector('.container');
+
+btn.addEventListener('click', function() {
+    
+    if(container.style.display === 'block') {
+       container.style.display = 'none'; 
+    } else {
+        container.style.display = 'block';
+    }
+});
+
+function esconder(){
+    var div = document.getElementById("teste");
+    div.style.display = 'none';
+}
