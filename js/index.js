65c5671c68ed5de3b9fa15d65c18e09fda86108e
Marcelo Cardoso Filho <marcelobuenocardosofilho@gmail.com>
2022-06-30 17:09:53 -0300

    js

--- js/index.js
+++ js/index.js
@@ -15,19 +15,35 @@ function esconder(){
     div.style.display = 'none';
 }
 /*======================================================================================*/
-//VARIÁVEIS
+//VARIÁVEIS BOLINHA
 let xbolinha= 300;
 let ybolinha= 240;
 let dbolinha= 15;
 let raio = dbolinha/2;
 
 //VELOCIDADE DA BOLINHA
-let sxbolinha= 8;
-let sybolinha= 8;
+let sxbolinha= 5;
+let sybolinha= 5;
+
+//VARÍAVEIS RAQUETE
+let xraquete = 20;
+let yraquete = 125;
+let craquete = 3;
+let araquete = 50; 
+
+//VARÍAVEIS CPU
+let xraquetecpu = 570;
+let yraquetecpu = 125;
+let sraquetecpu;
+
+//PLACAR 
+let pontos = 0;
+let pontoscpu = 0;
 
 
 function setup() {
-    createCanvas(800, 600);
+    var myCanvas = createCanvas(600, 300);
+    myCanvas.parent("fundinho");
   }
   
   function draw() {
@@ -35,6 +51,12 @@ function setup() {
     showbolinha();
     movebolinha();
     colisaoborda();
+    showraquete(xraquete, yraquete);
+    moveraquete();
+    colisaoraquete();
+    colisaoraquetecpu();
+    showraquete(xraquetecpu, yraquetecpu);
+    moveraquetecpu();
 }
 
 function showbolinha(){
@@ -50,9 +72,40 @@ function colisaoborda(){
     if (xbolinha + raio > width || xbolinha - raio < 0){
         sxbolinha*=-1;
     } 
-
     if(ybolinha + raio > height || ybolinha - raio < 0){
         sybolinha*=-1;
     }   
+}
+
+function showraquete(x,y){
+    rect(x, y, craquete, araquete);
+}
+
+function moveraquete(){
+    if (keyIsDown(UP_ARROW)){
+         yraquete -=10;
+    }
+    if (keyIsDown(DOWN_ARROW)){
+        yraquete +=10;
+   }
+}
+
+function colisaoraquete(){
+    if (xbolinha - raio < xraquete + craquete && 
+        ybolinha - raio < yraquete + araquete && 
+        ybolinha + raio > yraquete){
+        sxbolinha *= -1;
+    } 
+}
+
+function colisaoraquetecpu(){
+    if (xbolinha + raio > xraquetecpu && ybolinha + raio < yraquetecpu + araquete && ybolinha + raio > yraquetecpu){
+        sxbolinha*=-1;
+    } 
+}
+
+function moveraquetecpu(){
+    sraquetecpu = ybolinha - yraquetecpu - craquete / 2 - 30;
+    yraquetecpu += sraquetecpu;
 }
 
